979cf1fa5d48590d82d7b23108887ddd6b18ddfa
derickwarshaw <dwarshaw@madmobile.com>
2016-11-12 23:09:13 -0500

    - Updated File test to work with updated File component - Added key to <li> being iterated based on index - Broke out into separate tests to check for the component rendering and looking for the value of a specific element's text based on the props that hydrate - Added a test that checks for absence of array of componentProps

--- test/File.test.js
+++ test/File.test.js
@@ -5,24 +5,66 @@ import File from '../src/client/components/File';
 /**
  * Renders a File Component
  */
-it('renders a File component', () => {
+it('renders a File component and displays the name', () => {
   const props = {
-    comments: [
-      {
-        propTypes: [
-          {
-            name: 'name',
-            paramType: 'paramType',
-            description: 'A test description'
-          }
-        ]
-      }],
-    name: 'test'
+    componentProps: [
+      {name: 'name'},
+      {type: 'type'}],
+    filename: 'filename',
+    name: 'name'
   };
 
   const wrapper = mount(
     <File {...props} />
   );
 
-  expect(wrapper.find('h1').text()).toEqual('test');
+  expect(wrapper.find('h3').text()).toEqual('name');
+});
+
+it('renders a File component and displays the filename', () => {
+  const props = {
+    componentProps: [
+      {name: 'name'},
+      {type: 'type'}],
+    filename: 'filename',
+    name: 'name'
+  };
+
+  const wrapper = mount(
+    <File {...props} />
+  );
+
+  expect(wrapper.find('h4').text()).toEqual('filename');
+});
+
+it('renders a File component and displays component props name and type', () => {
+  const props = {
+    componentProps: [
+      {
+        name: 'name',
+        type: 'type'
+      }],
+    filename: 'filename',
+    name: 'name'
+  };
+
+  const wrapper = mount(
+    <File {...props} />
+  );
+
+  expect(wrapper.find('li').text()).toEqual(`${props.componentProps[0].name} - ${props.componentProps[0].type}`);
+});
+
+it('renders a File component and does not display any component props as list-items', () => {
+  const props = {
+    componentProps: [],
+    filename: 'filename',
+    name: 'name'
+  };
+
+  const wrapper = mount(
+    <File {...props} />
+  );
+
+  expect(wrapper.find('ul').text()).toEqual('');
 });
